0bff3e95acefd66484a335f17b2aad330329f491
92scoupe <131972382+92scoupe@users.noreply.github.com>
2023-05-02 15:03:36 +0900

    feat: add random num

--- bulls_game.py
+++ bulls_game.py
@@ -1,1 +1,14 @@
-print('hello world')
+# don't blame me if the code is not working
+
+import random
+
+random_num =[]
+
+for i in range(10):
+    num = random.randrange(1, 10)
+    if num not in random_num:
+        random_num.append(num)
+    else: 
+        print(random_num)
+
+       
